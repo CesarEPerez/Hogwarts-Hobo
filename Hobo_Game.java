db352308b6c8e403f907c17b807b176c954c2481
Jonathan Ong <39965757+JayceSoul@users.noreply.github.com>
2020-03-24 13:09:30 -0400

    Add files via upload
    Just a  blank GUI

---
+++ Hobo_Game.java
@@ -0,0 +1,113 @@
+import java.awt.*; //abstract windows toolkit used for gui objects
+import java.io.File; //for files
+import java.util.Collections; //Allows use of Collections
+import java.awt.event.*; //Allows use of events
+import javax.swing.*; //also used for gui objects
+import javax.swing.Timer; // Allow use of timer
+import javax.swing.event.*; // Allows use of events
+import javax.imageio.*; // for images
+import java.io.*; // for file reading/loading
+import java.io.FileNotFoundException; //exception error
+import sun.audio.*;     //sun.audio package imported (sound)     
+import java.util.ArrayList; //allows use of Arraylist  
+import java.awt.Color; //for color
+import java.awt.Graphics; //for drawing graphics
+
+
+
+public class Hobo_Game extends JFrame
+{// start
+//==================================<Audio Streams>===================================
+//Creating AudioStream objects (null is default declaration)
+  public static AudioStream sfx = null; // audiostream for short sound effects
+  public static AudioStream playing = null; // audiostream for music
+  Timer animation = new Timer(80,null);//manages transitions  
+
+//==================================<Image Method>====================================
+  public static Image loadImage (String name)  //Loads image from file
+  {//start
+    Image img = null;
+    try
+    {
+      img = ImageIO.read (new File (name));
+    }
+    catch (IOException e)
+    {
+    }
+    return img;
+  }//end 
+//==================================<Sound Method>====================================  
+  public static void Player (String audioname)  // Music method
+  {//start
+    InputStream in = null; // Readies input stream  for audio
+    try
+    {
+      in = new FileInputStream (new File (audioname)); // for importing Files into FileInputStream
+    }
+    catch (FileNotFoundException e)
+    {
+    }
+    try
+    {
+      playing = new AudioStream (in);
+    }
+    catch (IOException e)
+    {
+    }
+  }//end  
+
+//=================================<Panel constructor>============================== 
+  public Hobo_Game() 
+  {// start of panel setup
+    // Creating JPanel for the background and JPanel for interations (options)
+    setLayout (new BorderLayout ());// Use BorderLayout for main panel
+    JPanel background = new JPanel (); // Create a content panel to hold the backgournd images
+    background.setLayout(null);// sets layout to null so that set bounds can be used 
+    // timer setup   
+    
+    // Colouring panels  
+    setBackground(Color.BLACK);
+    //timer setup   
+  
+    // Creating Drawing Area and Panel Setup
+    DrawArea playfield = new DrawArea (1080, 740);
+    setContentPane(background);
+    background.setFocusable(true);
+//--------------------------------------------------------------------        
+ 
+//--------------------------------------------------------------------
+    pack();
+    // Game window Setup  
+    setTitle("Hobo Gasme");
+    background.setPreferredSize (new Dimension (1080,740));
+    setSize(1080,740); //Sets the JFrame size
+    setVisible(true); //Reveals JFrame
+    setResizable(false); 
+    setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+    setLocationRelativeTo(null);//centers window on launch    
+  }//end of panel setup
+//========================================<Draw class>======================================= 
+  class DrawArea extends JPanel
+  {//start of drawarea
+    // load images for use
+  
+    
+    public DrawArea (int width, int height)// Create panel of given size
+    {//start of drawarea
+      this.setBounds( 0, 0, width, height);//(new Dimension (width, height));
+    }//end of drawarea
+    
+    public void paintComponent (Graphics g)  // g can be passed to a class method
+    {//start of paintComponent
+     
+    }//end
+   }///end of class
+//----------------------------------------------------------------------------------------
+ 
+//================================================================================== 
+  public static void main(String[] args)
+  {///start of main 
+    Hobo_Game game = new  Hobo_Game();
+    game.setVisible (true);
+  }//end of main
+}///end of class
