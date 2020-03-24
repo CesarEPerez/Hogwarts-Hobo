9fca016a2925502945f36cc77cbfadb1de167ce8
Jayce Soul <xxJayceSoulxx@gmail.com>
2020-03-24 18:12:20 -0400

    Basic track object constuctor

---
+++ Track.java
@@ -0,0 +1,30 @@
+public class Track {
+
+    boolean trainOn; // if train is on track
+    int id, // the track id so that we know which one 
+    hobos; // how many hobos on the current track
+    public Track (int x) {
+        id = x;
+        trainOn = false;
+        hobos = 0;
+    }
+
+    //set hobos to number
+    public void setHobos(int hobos) {
+        this.hobos = hobos;
+    }
+
+    //
+    public void setTrainOn(boolean trainOn) {
+        this.trainOn = trainOn;
+    }
+
+    public int getHobos() {
+        return hobos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+}
